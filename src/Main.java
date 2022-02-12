6a7197cc99b7cd3b87a5b05fb7143e7e5d3b72d9
SDT-Ilya-sh <ilya.shvajbovich@mail.ru>
2022-02-12 12:35:00 +0300

    firstCommit

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,8 @@ public class Main {
     public static void main(String[] args)
     {
         int a;
+        int gitCheck;
+        boolean gitProverka;
         double incomeNeedForInvestments = 0.1;
         while (true) {
             System.out.println("Введите сумму доходов компании за месяц " +

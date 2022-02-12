a3a695bf87dfa9ec0d1473a236042c34181ec6b8
SDT-Ilya-sh <ilya.shvajbovich@mail.ru>
2022-02-12 13:59:24 +0300

    createdHomeTask

---
+++ src/lesson1/Loader.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat cat = new Cat();
+
+        System.out.println(cat.getStatus());
+    }
+}

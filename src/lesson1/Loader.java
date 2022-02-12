05affd07a3aae4124ecf0c7f790ff06d6b47d325
SDT-Ilya-sh <ilya.shvajbovich@mail.ru>
2022-02-12 14:14:22 +0300

    Proverka Zheki

--- src/lesson1/Loader.java
+++ src/lesson1/Loader.java
@@ -5,7 +5,7 @@ public class Loader
     public static void main(String[] args)
     {
         Cat cat = new Cat();
-
+        System.out.println("Proverka Zheki");
         System.out.println(cat.getStatus());
     }
 }

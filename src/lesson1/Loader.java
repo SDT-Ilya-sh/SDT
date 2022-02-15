e7850b55049591238d4d6e0f196c08bd527681f5
SDT-Ilya-sh <ilya.shvajbovich@mail.ru>
2022-02-15 15:41:34 +0300

    Get Kitten

--- src/lesson1/Loader.java
+++ src/lesson1/Loader.java
@@ -2,28 +2,20 @@ package lesson1;
 
 public class Loader
 {
+    private static Cat getKitten()
+    {
+        Cat cat = new Cat(1100.00);
+        return cat;
+
+    }
     public static void main(String[] args)
     {
-        Cat dushnila = new Cat();
-        Cat zheka = new Cat();
-        Cat student = new Cat();
-        Cat timofey = new Cat();
 
-
-       // System.out.println("Dushnila in this moment - " + dushnila.getStatus());
-       // System.out.println("Student in this moment - " + student.getStatus());
-      // System.out.println("Timofey in this moment - " + timofey.getStatus());
-        System.out.println("Zheka in this moment - " + zheka.getStatus());
-        zheka.feed(100000.0);
-        zheka.feed(10.0);
-        //System.out.println(dushnila.getWeight());
-        //System.out.println("Dushnila in this moment "+dushnila.getStatus());
-        //System.out.println("Cats amount now - "+ Cat.getCount());
-        //Придумайте способ, который поможет проверить,
-        // жива ли кошка, на основе значения её веса.
-        // Используя это, запретите неживой кошке есть,
-        // пить, ходить в туалет, выполняя проверку веса
-        // внутри методов, которые изменяют вес кошки.
+        getKitten();
+        getKitten();
+        System.out.println(getKitten().getWeight());
+        System.out.println(getKitten().getStatus());
+        System.out.println(Cat.getCount());
 
     }
 }

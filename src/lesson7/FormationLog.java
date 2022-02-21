cb606821247c2ae269024dbe6f1866b47c5e3515
SDT-Ilya-sh <ilya.shvajbovich@mail.ru>
2022-02-21 18:39:17 +0300

    Final Version of a Truck Task

--- src/lesson7/FormationLog.java
+++ src/lesson7/FormationLog.java
@@ -5,51 +5,49 @@ public class FormationLog {
     private int boxNumber = 1;
     private int truckNumber = 1;
 
+    Truck truckColumn = new Truck();
+    Container container = new Container();
+    Box box = new Box();
+
 
     public void Formation(int truckCapacity, int contCapacity, int boxAmount) {
-        Truck truckColumn = new Truck();
-        Container container = new Container();
-        Box box = new Box();
         truckColumn.setTruckCapacity(truckCapacity);
         container.setContCapacity(contCapacity);
         box.setBoxAmount(boxAmount);
         container.setContAmount(box.getBoxAmount() / container.getContCapacity());
-        newFormation(truckColumn,container,box);
+        newFormation();
     }
 
 
-        public void newFormation (Truck truckColumn, Container container, Box box){
-            for (int i = 0; i <= getTruckAmount(truckColumn, container); i++) {
-                System.out.println("Грузовик номер " + truckNumber);
-                truckNumber++;
-                for (int j = 1; j <= truckColumn.getTruckCapacity(); j++) {
-                    System.out.println("  Контейнер номер " + contNumber);
+    public void newFormation() {
+        for (int i = 0; i <= getTruckAmount(truckColumn, container); i++) {
+            System.out.println("Грузовик номер " + truckNumber);
+            truckNumber++;
+            for (int j = 1; j <= truckColumn.getTruckCapacity(); j++) {
+                System.out.println("  Контейнер номер " + contNumber);
 
-                    for (int k = 1; k <= container.getContCapacity(); k++) {
-                        System.out.println("     Ящик номер " + boxNumber);
-                        if (boxNumber == box.getBoxAmount()) {
-                            return;
-                        }
-                        boxNumber++;
+                for (int k = 1; k <= container.getContCapacity(); k++) {
+                    System.out.println("     Ящик номер " + boxNumber);
+                    if (boxNumber == box.getBoxAmount()) {
+                        return;
                     }
-                    contNumber++;
+                    boxNumber++;
                 }
+                contNumber++;
             }
-
         }
 
+    }
 
 
     public int getContAmount(Container container) {
-        int contAmount = (int) Math.ceil(container.getContAmount());
-        return contAmount;
+        return (int) Math.ceil(container.getContAmount());
 
     }
 
     public int getTruckAmount(Truck truck, Container container) {
         int contAmount = (int) Math.ceil(container.getContAmount());
-        int truckAmount = (int) Math.ceil(getContAmount(container) / truck.getTruckCapacity());
-        return truckAmount;
+        return (int) Math.ceil(getContAmount(container) / truck.getTruckCapacity());
     }
 
 }

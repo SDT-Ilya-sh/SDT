c91b285861694a0acb6649087efd9b45571d5336
SDT-Ilya-sh <ilya.shvajbovich@mail.ru>
2022-02-22 14:38:37 +0300

    Code Printing Task

---
+++ src/lesson8/Main.java
@@ -0,0 +1,8 @@
+package lesson8;
+
+public class Main {
+    public static void main(String[] args) {
+    NewString string = new NewString();
+    string.codePrinting("Жека читает мой код");
+    }
+}
---
+++ src/lesson8/NewString.java
@@ -0,0 +1,10 @@
+package lesson8;
+
+public class NewString {
+    public void codePrinting(String string) {
+        int stringLength = string.length();
+        for (int i = 0; i < stringLength; i++) {
+            System.out.println("Символ - " + string.charAt(i) + ", Его код - " +(int) string.charAt(i));
+        }
+    }
+}
